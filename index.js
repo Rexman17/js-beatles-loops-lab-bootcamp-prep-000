02397c7ac5bcd6786560b8a12985c17705e9ae82
Ali Stone <a.stone1794@gmail.com>
2018-08-10 16:16:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musiciansArray, instrumentsArray) {
   for (let i = 0; i < musiciansArray.length; i++) {
-    const newArray = newArray.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`);
+    var newArray = newArray.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`);
   }
   return newArray;
 }
